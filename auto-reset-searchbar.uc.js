82be1ffcc0ee8d8872ddf60eeb66eebfde40108f
oflow <apps@oflow.me>
2014-12-09 22:44:11 +0900

    定数名プレフィックス

--- auto-reset-searchbar.uc.js
+++ auto-reset-searchbar.uc.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           auto reset searchbar
 // @description    Web検索したらキーワードをクリアして一番上の検索エンジンに戻す
-// @version        1.2.1
+// @version        1.2.2
 // @author         oflow
 // @compatibility  Firefox 31(ESR), 34
 // @namespace      http://oflow.me/archives/337
@@ -9,34 +9,34 @@
 
 (function() {
     // リセットするまでにちょっと時間をおくかどうか [true=タイマー使う, false=即時クリア]
-    const USE_TIMER = true;
+    const ARS_USE_TIMER = true;
     // リセットするまでの時間(ミリ秒) [3000=3秒]
-    const TIMER_MS  = 3000;
+    const ARS_TIMER_MS  = 3000;
     // 検索したら一番上の検索エンジンに戻すかどうか [true=戻す, false=そのまま]
-    const USE_DEFAULT_ENGINE = true;
+    const ARS_USE_DEFAULT_ENGINE = true;
     // リセットタイマーが動いてるときに検索バーにフォーカスでタイマーを止めるかどうか [true=止める, false=止めない]
-    const USE_FOCUS = true;
+    const ARS_USE_FOCUS = true;
     // 検索ボタンクリックでクリアするかどうか [true=クリア, false=そのまま]
-    const USE_CLICK = true;
+    const ARS_USE_CLICK = true;
     // 貼り付けて検索のときにクリアするかどうか [true=クリア]
-    const USE_COMMAND = true;
+    const ARS_USE_COMMAND = true;
     // Enterキーで検索のときにクリアするかどうか [true=クリア]
-    const USE_ENTER = true;
+    const ARS_USE_ENTER = true;
 
     var ucjsAutoResetSearchbar = {
         timer: null,
         init: function() {
             var searchbar = BrowserSearch.searchBar;
-            if (USE_FOCUS) {
+            if (ARS_USE_FOCUS) {
                 searchbar.addEventListener('focus', this, false);
             }
-            if (USE_CLICK) {
+            if (ARS_USE_CLICK) {
                 searchbar.addEventListener('click', this, false);
             }
-            if (USE_COMMAND) {
+            if (ARS_USE_COMMAND) {
                 searchbar.addEventListener('command', this, false);
             }
-            if (USE_ENTER) {
+            if (ARS_USE_ENTER) {
                 searchbar.addEventListener('keypress', this, false);
             }
         },
@@ -44,7 +44,7 @@
             switch (e.type) {
                 case 'focus':
                     // タイマーでリセットする前に検索バーにフォーカスしたらリセットしない
-                    if (USE_TIMER && this.timer) {
+                    if (ARS_USE_TIMER && this.timer) {
                         clearTimeout(this.timer);
                         this.timer = null;
                     }
@@ -79,20 +79,22 @@
             if (!textbox) {
                 return;
             }
-            if (USE_TIMER && this.timer) {
+            if (ARS_USE_TIMER && this.timer) {
                 clearTimeout(this.timer);
                 this.timer = null;
             }
-            if (USE_TIMER) {
+            if (ARS_USE_TIMER) {
                 this.timer = setTimeout(function() {
                     textbox.value = '';
-                    if (USE_DEFAULT_ENGINE) {
+                    if (ARS_USE_DEFAULT_ENGINE) {
                         searchbar.currentEngine = searchbar.engines[0];
                     }
-                }, TIMER_MS);
+                }, ARS_TIMER_MS);
             } else {
                 textbox.value = '';
-
+                if (ARS_USE_DEFAULT_ENGINE) {
+                    searchbar.currentEngine = searchbar.engines[0];
+                }
             }
         }
     }
