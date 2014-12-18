1ac34d3057d9a6afc0874711fa003727edc596b9
oflow <apps@oflow.me>
2014-12-18 16:21:47 +0900

    remove arguments.callee

--- search-engine-select-and-paste-and-search.uc.js
+++ search-engine-select-and-paste-and-search.uc.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Search Engine middle-click "Select and Paste and Search"
 // @description    検索エンジンメニューのミドルクリックで検索エンジンを切り替えてから貼り付けて検索
-// @version        1.1
+// @version        1.2
 // @include        main
 // @compatibility  Firefox ESR31.3
 // @author         oflow
@@ -12,20 +12,20 @@
     var ucjsSearchEngineMiddleClickSearch = {
         searchbar: BrowserSearch.searchBar,
         init: function() {
-            var searchbar = this.searchbar;
-            if (!searchbar) {
+            if (!this.searchbar) {
                 return;
             }
-            searchbar.addEventListener('click', this, false);
-            window.addEventListener('beforeunload', function() {
-                searchbar.removeEventListener('click', this, false);
-                window.removeEventListener('beforeunload', arguments.callee, false);
-            }, false);
+            this.searchbar.addEventListener('click', this, false);
+            window.addEventListener('unload', this, false);
+        },
+        uninit: function() {
+            this.searchbar.removeEventListener('click', this, false);
+            window.removeEventListener('unload', this, false);
         },
         handleEvent: function(e) {
-            var target = e.originalTarget;
             switch (e.type) {
                 case 'click':
+                    var target = e.originalTarget;
                     if (e.button == 1) {
                         if (target.nodeName == 'menuitem') {
                             this.paste();
@@ -38,6 +38,9 @@
                     }
 
                     break;
+                case 'unload':
+                    this.uninit();
+                    break;
             }
         },
         paste: function() {
@@ -47,7 +50,7 @@
         select: function(menuitem) {
             var name      = menuitem.label,
                 searchbar = this.searchbar,
-                engines   = searchbar.engines;
+                engines   = searchbar._engines || searchbar.engines;
 
             for (var i = 0; i < engines.length; i++) {
                 if (name == engines[i].name) {
